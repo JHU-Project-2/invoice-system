0ea3d02850e5d809834bdfc91fe4a2f212d4e8c6
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2021-09-04 11:25:30 -0400

    added logos onto different pages, added some spacing between different forms

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,17 @@
 let logoutButton = document.querySelector(".loggin_out");
+let darkLogo = document.querySelector(".dark-logo");
 
 if (window.location.pathname === "/" || window.location.pathname === "/logout") {
     logoutButton.setAttribute("style", "color: white!important;")
 } else {
     logoutButton.setAttribute("style", "color: black!important;")
+};
+
+if (window.location.pathname === "/") {
+    darkLogo.setAttribute("style", "display: none;");
+} else if (window.location.pathname === "/login") {
+    darkLogo.setAttribute("style", "display: block; width: 30%; float: left; margin-left: 65vh; margin-top: 15vh;" )
+} else if (window.location.pathname === "/dashboard/invoice/1" || window.location.pathname === "/dashboard/add-company") {
+    darkLogo.setAttribute("style", "display: block; width: 10%; margin-top: 40vh;")
 }
+
