4237989774213ee273ae16bac88d1db7596de4e3
Andrew Keiser <webdev410@gmail.com>
2021-09-07 13:13:16 -0700

    fixed line total function

--- public/js/line-total.js
+++ public/js/line-total.js
@@ -1,16 +1,16 @@
-let ppu = document.querySelector('.ppu').innerHTML
-let units = document.querySelector('.units').innerHTML
+let ppu = document.querySelectorAll('.ppu')
+let units = document.querySelectorAll('.units')
 let row = document.querySelectorAll('.line-item')
 let totalEl = document.querySelectorAll('.total')
 
-// console.log(totalEl)
+
 
 
 
 function init() {
     // each line total 
     for (var i = 0; i < row.length; i++) {
-        let lineTotal = ppu * units
+        let lineTotal = ppu[i].innerHTML * units[i].innerHTML
         totalEl[i].append(lineTotal)
         // totalEl.append(lineTotal)
         // console.log(lineTotal)
