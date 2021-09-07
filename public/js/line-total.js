dd8710e372df2464887ec5b6aae3d926c7d47f13
Andrew Keiser <webdev410@gmail.com>
2021-09-07 13:41:29 -0700

    invoice total is working

--- public/js/line-total.js
+++ public/js/line-total.js
@@ -2,22 +2,26 @@ let ppu = document.querySelectorAll('.ppu')
 let units = document.querySelectorAll('.units')
 let row = document.querySelectorAll('.line-item')
 let totalEl = document.querySelectorAll('.total')
+let invoiceTotal = document.querySelector('.invoice-total')
+let lineTotal;
+let totalArray = [];
 
-
-
-
-
-function init() {
-    // each line total 
+function getTotal() {
+    // get each line total 
     for (var i = 0; i < row.length; i++) {
-        let lineTotal = ppu[i].innerHTML * units[i].innerHTML
+        lineTotal = ppu[i].innerHTML * units[i].innerHTML
         totalEl[i].append(lineTotal)
-        // totalEl.append(lineTotal)
-        // console.log(lineTotal)
-    }
+        console.log(lineTotal)
 
+        totalArray.push(lineTotal)
+    }
+    // add all line totals and append to invoice
+    totalSum = totalArray.reduce((a, b) => a + b, 0)
+    invoiceTotal.append(totalSum)
 
 }
 
 
-init()
+
+
+getTotal()
